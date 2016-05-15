aae385074e4caffd7d6c284f7bdce7f39a3a5cd8
conrad <whalliburton@gmail.com>
2016-05-15 15:57:09 -0600

    getting there

--- main.lisp
+++ main.lisp
@@ -5,8 +5,7 @@
                          :imports ("style")
                          :scripts (("hack.js" hack-js) ("logo.js" logo-js))
                          :package :hackathon
-                         :modules (:roboto
-                                   :page
+                         :modules (:roboto :page :packery
                                    :polymer
                                    :iron-flex-layout :iron-icons
                                    :neon-animatable :neon-animated-pages :fade-in-animation :fade-out-animation
@@ -14,11 +13,12 @@
                                    :paper-icon-button
                                    :google-map)
                          :page-args (:body-class "fullbleed layout vertical"))
-  (:style :is "custom-style" :include "iron-flex iron-flex-alignment")
+  (:style :is "custom-style" :include "iron-flex iron-flex-alignment iron-positioning")
   (animated-pages :id "pages" :class "flex" :style "padding:20px;"
     :entry-animation "fade-in-animation"
     :exit-animation "fade-out-animation"
     :selected 0
+    (animatable) ; initial loading shows and transitions to next
     (animatable :id "top-grid"
       (card :elevation 5 :class "card press-release" :style "cursor:pointer;" :onclick "page(\"/press-release\")"
         (:div :class "card-content layout vertical center" :style "padding:30px;"
@@ -26,7 +26,7 @@
               (ripple)
               (button :onclick "page(\"/press-release\"); " "Press Release"))))
     (animatable (render-press-release stream)))
-  (script (when-ready (lambda () (setup-routing) (animate-logo)))))
+  (script (when-ready (lambda () (setup-routing) (animate-logo) (setup-packing "top-grid" "card")))))
 
 (in-package :story-js)
 
@@ -36,6 +36,6 @@
       (unless (=== (@ pages selected) index)
         (setf (@ pages selected) index))))
   (defun setup-routing ()
-    (page "/" (lambda () (select-page 0)))
-    (page "/press-release" (lambda () (select-page 1)))
+    (page "/" (lambda () (select-page 1)))
+    (page "/press-release" (lambda () (select-page 2)))
     (page (create :hashbang t))))
